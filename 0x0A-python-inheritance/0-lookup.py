87b529f3ffe1fb5a003e4e8c197f08f0027f9f09
Moses Ekwunife <mosesekwunife@yahoo.com>
2023-04-19 06:41:20 +0100

    0x0A-python-inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Program lookup all attributes and methods of an object."""
+"""Defines an object attribute lookup function."""
+
 
 def lookup(obj):
     """Return a list of an object's available attributes."""
